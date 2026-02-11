849de00e3a036fa02b52222dd11151e60d42977e
sachin-tyagi80 <sachintyagi6322@gmail.com>
2026-02-11 22:43:05 +0530

    Pair with Given Sum

---
+++ pairSum.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class pairSum{
+
+    static void checkSum(int n, int k, int[] a) {
+
+        Arrays.sort(a);   // sort array
+
+        int left = 0;
+        int right = n - 1;
+
+        while(left < right) {
+
+            int sum = a[left] + a[right];
+
+            if(sum == k) {
+                System.out.println("Yes");
+                return;
+            }
+            else if(sum < k) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+
+        System.out.println("No");
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();   // size of array
+        int k = scanner.nextInt();   // target sum
+
+        int[] a = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+
+        checkSum(n, k, a);
+
+        scanner.close();
+    }
+}
